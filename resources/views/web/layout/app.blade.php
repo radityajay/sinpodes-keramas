022e170e779df82fa1ef040a0d2a3f4091700b16
YudhaArdana <yudhaardana10@gmail.com>
2022-11-13 15:43:07 +0800

    update at web

---
+++ resources/views/web/layout/app.blade.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>@yield('title')</title>
+    <!-- Font Icons -->
+    <link media="all" rel="stylesheet" href="/assets/web/css/fonts/icomoon/icomoon.css">
+<!--     <link media="all" rel="stylesheet" href="css/fonts/roxine-font-icon/roxine-font.css"> -->
+    <link media="all" rel="stylesheet" href="/assets/web/vendors/font-awesome/css/font-awesome.css">
+    <!-- Vendors -->
+    <link media="all" rel="stylesheet" href="/assets/web/vendors/owl-carousel/dist/assets/owl.carousel.min.css">
+    <link media="all" rel="stylesheet" href="/assets/web/vendors/owl-carousel/dist/assets/owl.theme.default.min.css">
+    <link media="all" rel="stylesheet" href="/assets/web/vendors/animate/animate.css">
+    <link media="all" rel="stylesheet" href="/assets/web/vendors/rateyo/jquery.rateyo.css">
+    <link media="all" rel="stylesheet" href="/assets/web/vendors/bootstrap-datepicker/css/bootstrap-datepicker.css">
+    <link media="all" rel="stylesheet" href="/assets/web/vendors/fancyBox/source/jquery.fancybox.css">
+    <link media="all" rel="stylesheet" href="/assets/web/vendors/fancyBox/source/helpers/jquery.fancybox-thumbs.css">
+    <!-- Bootstrap 4 -->
+    <link media="all" rel="stylesheet" href="/assets/web/css/bootstrap.css">
+    <!-- Rev Slider -->
+    <link rel="stylesheet" type="text/css" href="/assets/web/vendors/rev-slider/revolution/css/settings.css">
+    <link rel="stylesheet" type="text/css" href="/assets/web/vendors/rev-slider/revolution/css/layers.css">
+    <link rel="stylesheet" type="text/css" href="/assets/web/vendors/rev-slider/revolution/css/navigation.css">
+    <!-- Theme CSS -->
+    <link media="all" rel="stylesheet" href="/assets/web/css/main.css">
+    <!-- Custom CSS -->
+    <link media="all" rel="stylesheet" href="/assets/web/css/custom.css">
+</head>
+
+<body class="white-overlay">
+    <div class="preloader" id="pageLoad">
+        <div class="holder">
+            <div class="coffee_cup"></div>
+        </div>
+    </div>
+    <!--Side panel-->
+    @include('web.layout.sidebar')
+    <!-- main wrapper -->
+    <div id="wrapper" class="no-overflow-x">
+        <div class="page-wrapper">
+            <!-- header of the page -->
+            @include('web.layout.navbar')
+            <!--/header of the page -->
+            <main>
+                @yield('content')
+            </main>
+        </div>
+        <!-- footer of the pagse -->
+        <footer class="footer footer-v1">
+            <div class="content-block footer-main">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="footer-logo">
+                                <img src="img/logo-dark.svg" alt="image-description">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="footer-nav inline-nav text-center">
+                                <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-4 text-right">
+                            <ul class="social-network with-text">
+                                <li><a href="#"><span class="icon-facebook"></span></a></li>
+                                <li><a href="#"><span class="icon-twitter"></span> </a></li>
+                                <li><a href="#"><span class="icon-google-plus"></span> </a></li>
+                                <li><a href="#"><span class="icon-pinterest"></span> </a></li>
+                                <li><a href="#"><span class="icon-dribbble"></span> </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom text-center">
+                <div class="container">
+                    <p>Copyright 2016 - Roxine - Multi Purpose Theme by Waituk </p>
+                </div>
+            </div>
+        </footer>
+        <!--/footer of the page -->
+    </div>
+    <!-- open/close -->
+    <div class="search-form-wrapper overlay overlay-hugeinc">
+        <button type="button" class="overlay-close"><span class="custom-icon-plus"></span></button>
+        <div class="search-form">
+            <form action="#" method="post">
+                <div class="form-group">
+                    <input type="search" class="form-control" placeholder="Enter Your Search">
+                    <button type="submit"><span class="icon-search"></span></button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <a href="#" class="section-scroll" id="scroll-to-top"><i class="fa fa-angle-up"></i></a>
+    <!-- jQuery Library -->
+    <script src="/assets/web/vendors/jquery/jquery-2.1.4.min.js"></script>
+    <!-- Vendor Scripts -->
+    <script src="/assets/web/vendors/tether/dist/js/tether.min.js"></script>
+    <script src="/assets/web/vendors/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/assets/web/vendors/stellar/jquery.stellar.min.js"></script>
+    <script src="/assets/web/vendors/isotope/javascripts/isotope.pkgd.min.js"></script>
+    <script src="/assets/web/vendors/isotope/javascripts/packery-mode.pkgd.js"></script>
+    <script src="/assets/web/vendors/owl-carousel/dist/owl.carousel.min.js"></script>
+    <script src="/assets/web/vendors/waypoint/waypoints.min.js"></script>
+    <script src="/assets/web/vendors/counter-up/jquery.counterup.min.js"></script>
+    <script src="/assets/web/vendors/fancyBox/source/jquery.fancybox.pack.js"></script>
+    <script src="/assets/web/vendors/fancyBox/source/helpers/jquery.fancybox-thumbs.js"></script>
+    <script src="/assets/web/vendors/image-stretcher-master/image-stretcher.js"></script>
+    <script src="/assets/web/vendors/wow/wow.min.js"></script>
+    <script src="/assets/web/vendors/rateyo/jquery.rateyo.min.js"></script>
+    <script src="/assets/web/vendors/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+    <script src="/assets/web/vendors/bootstrap-slider-master/src/js/bootstrap-slider.js"></script>
+    <script src="/assets/web/vendors/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+    <script src="js/mega-menu.js"></script>
+    <script src="/assets/web/vendors/retina/retina.min.js"></script>
+    <!-- Custom Script -->
+    <script src="/assets/web/js/jquery.main.js"></script>
+    <!-- REVOLUTION JS FILES -->
+    <script type="text/javascript" src="/assets/web/vendors/rev-slider/revolution/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="/assets/web/vendors/rev-slider/revolution/js/jquery.themepunch.revolution.min.js"></script>
+    <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+    <script type="text/javascript" src="/assets/web/vendors/rev-slider/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+    <script type="text/javascript" src="/assets/web/vendors/rev-slider/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+    <script type="text/javascript" src="/assets/web/vendors/rev-slider/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+    <script type="text/javascript" src="/assets/web/vendors/rev-slider/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script type="text/javascript" src="/assets/web/vendors/rev-slider/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+    <script type="text/javascript" src="/assets/web/vendors/rev-slider/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script type="text/javascript" src="/assets/web/vendors/rev-slider/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+    <script type="text/javascript" src="/assets/web/vendors/rev-slider/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script type="text/javascript" src="/assets/web/vendors/rev-slider/revolution/js/extensions/revolution.extension.video.min.js"></script>
+    <!-- SNOW ADD ON -->
+    <script type="text/javascript" src="/assets/web/vendors/rev-slider/revolution-addons/snow/revolution.addon.snow.min.js"></script>
+    <!-- Revolution Slider Script -->
+    <script src="/assets/web/js/revolution.js"></script>
+</body>
+
+</html>
---
+++ resources/views/web/layout/navbar.blade.php
@@ -0,0 +1,160 @@
+<header class="fixed-top main-header header-white transparent with-side-panel-ico" id="waituk-main-header">
+                <div id="nav-section">
+                    <div class="bottom-header container-fluid mega-menus" id="mega-menus">
+                        <nav class="navbar navbar-toggleable-md no-border-radius no-margin mega-menu-multiple"
+                            id="navbar-inner-container">
+                            <form action="mega-menu-5.html" id="top-search" class="no-margin top-search">
+                                <div class="form-group no-margin">
+                                    <input class="form-control no-border" id="search_term" name="search_term"
+                                        placeholder="Type & Press Enter" type="text">
+                                </div>
+                            </form>
+                            <button type="button" class="navbar-toggler navbar-toggler-left" data-toggle="collapse"
+                                data-target="#mega-menu">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <a class="navbar-brand mr-auto m-sm-auto" href="index.html"> <img src="/assets/web/img/logoo.png"
+                                    alt="keramas" style="height: 40px !important"> <img src="/assets/web/img/logoo.png"
+                                    alt="keramas" style="height: 40px !important"> </a>
+                            <div class="collapse navbar-collapse flex-row-reverse" id="mega-menu">
+                                <ul class="nav navbar-nav">
+                                    <li class="dropdown" data-animation="fadeIn">
+                                        <a href="index.html" data-title="Home"> Home </a>
+                                    </li>
+                                    <li class="dropdown" data-animation="fadeIn">
+                                        <a href="about.html" data-title="Profil Desa"> Profil Desa </a>
+                                    </li>
+                                    <li class="dropdown" data-animation="fadeIn">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" href="index.html"
+                                            data-title="Home"> Pemerintahan </a>
+                                        <ul class="dropdown-menu no-border-radius">
+                                            <li class="dropdown dropdown-left dropdown-parent">
+                                                <a href="perangkat-desa.html"> Perangkat Desa</a>
+                                            </li>
+                                            <li class="dropdown dropdown-left dropdown-parent">
+                                                <a href="pkk.html"> PKK</a>
+                                            </li>
+                                            <li class="dropdown dropdown-left dropdown-parent">
+                                                <a href="linmas.html"> Linmas</a>
+                                            </li>
+                                            <li><a href="karang-taruna.html"> Karang Taruna </a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown" data-animation="fadeIn">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" href="index.html"
+                                            data-title="Home"> Artikel </a>
+                                        <ul class="dropdown-menu no-border-radius">
+                                            <li class="dropdown dropdown-left dropdown-parent">
+                                                <a href="pengumuman.html"> Pengumuman</a>
+                                            </li>
+                                            <li class="dropdown dropdown-left dropdown-parent">
+                                                <a href="berita.html"> Berita</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="dropdown" data-animation="fadeIn">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" href="index.html"
+                                            data-title="Home"> Potensi Desa </a>
+                                        <ul class="dropdown-menu no-border-radius">
+                                            <li class="dropdown dropdown-left dropdown-parent">
+                                                <a href="wisata.html"> Pariwisata</a>
+                                            </li>
+                                            <li class="dropdown dropdown-left dropdown-parent">
+                                                <a href="alam.html"> Alam</a>
+                                            </li>
+                                            <li class="dropdown dropdown-left dropdown-parent">
+                                                <a href="senbud.html"> Seni Budaya</a>
+                                            </li>
+                                            <li class="dropdown dropdown-left dropdown-parent">
+                                                <a href="kuliner.html"> Kuliner</a>
+                                            </li>
+                                            <li class="dropdown dropdown-left dropdown-parent">
+                                                <a href="perkebunan.html"> Perkebunan</a>
+                                            </li>
+                                            <li class="dropdown dropdown-left dropdown-parent">
+                                                <a href="perikanan.html"> Perikanan</a>
+                                            </li>
+                                            <li class="dropdown dropdown-left dropdown-parent">
+                                                <a href="kerajinan.html"> Kerajinan</a>
+                                            </li>
+                                            <li class="dropdown dropdown-left dropdown-parent">
+                                                <a href="usaha-mikro.html"> Usaha Mikro</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <!-- <li>
+                                        <a href="about.html"> About </a>
+                                    </li>
+                                    <li>
+                                        <a href="team.html"> Team </a>
+                                    </li> -->
+                                    <li class="dropdown right-dropdown" data-animation="fadeIn">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" href="index.html"
+                                            data-title="Home"> Produk Hukum </a>
+                                        <ul class="dropdown-menu no-border-radius">
+                                            <li><a href="perdes.html"> Peraturan Desa </a></li>
+                                            <li><a href="perkab.html"> Peraturan Kabupaten </a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="contact.html"> APBDesa </a>
+                                    </li>
+                                    <li>
+                                        <a href="pengaduan.html"> Pengaduan Masyarakat </a>
+                                    </li>
+                                    <!-- <li class="dropdown cart-list margin-0-sm">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" href="javascript:void(0);"><span class="hidden-lg-up"> Shopping Cart </span><i class="custom-icon-cart m-marker"></i></a>
+                                        <div class="dropdown-menu-container">
+                                            <div class="dropdown-menu no-border-radius col-lg-1 col-md-4 col-sm-4">
+                                                <h4> Shopping Cart </h4>
+                                                <ul class="s-list no-padding">
+                                                    <li class="row">
+                                                        <div class="col-lg-4 col-md-4 col-sm-5 col">
+                                                            <div class="img">
+                                                                <a href="#"><img src="img/img-02.jpg" alt="image description"></a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-lg-8 col-md-8 col-sm-7 col">
+                                                            <h6> <a href="#"><strong>Product 1 </strong></a><span class="text-muted float-right"> x 1 </span></h6>
+                                                            <h6> $199 </h6>
+                                                        </div>
+                                                    </li>
+                                                    <li class="divider"></li>
+                                                    <li class="row">
+                                                        <div class="col-lg-4 col-md-4 col-sm-5 col">
+                                                            <div class="img">
+                                                                <a href="#"><img src="img/img-02.jpg" alt="image description"></a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-lg-8 col-md-8 col-sm-7 col">
+                                                            <h6> <a href="#"><strong>Product 2 </strong></a><span class="text-muted float-right"> x 2 </span></h6>
+                                                            <h6> $199 </h6>
+                                                        </div>
+                                                    </li>
+                                                    <li class="divider margin-bottom-1"></li>
+                                                    <li class="row">
+                                                        <div class="col-lg-4 col-md-4 col-sm-5 col">
+                                                            <h2> Total </h2>
+                                                        </div>
+                                                        <div class="col-lg-8 col-md-8 col-sm-7 col">
+                                                            <h2 class="text-right"> $3300 </h2>
+                                                        </div>
+                                                    </li>
+                                                    <li><a href="#" class="btn btn-sm btn-block btn-secondary btn-mina btn-mina-rip-br"> View Cart </a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </li> -->
+                                </ul>
+                            </div>
+                            <!-- <div class="navbar-pos-search with-side-panel">
+                                <a href="#" class="x-search x-search-trigger navbar-link"><i class="custom-icon-search"></i></a>
+                                <a href="#" class="x-search icon-close-round navbar-link"><i class="icon-line-cross"></i></a>
+                            </div>
+                            <div class="nav-trigger navbar-pos-search overlay-trigger">
+                                <a href="#" class="navbar-link"><i class="icon-sort-1"></i></a>
+                            </div> -->
+                        </nav>
+                    </div>
+                </div>
+            </header>
