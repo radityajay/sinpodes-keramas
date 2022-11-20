196034d4f35ee0d4ca8c37ab1981f076bac5c096
radityajay <radityajayantara76@gmail.com>
2022-11-20 16:58:17 +0800

    update dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,7 +2,25 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Alam;
+use App\Announcement;
 use App\Http\Controllers\Controller;
+use App\KarangTaruna;
+use App\Kerajinan;
+use App\Kuliner;
+use App\Linmas;
+use App\News;
+use App\Pariwisata;
+use App\PerangkatDesa;
+use App\Perikanan;
+use App\Perkebunan;
+use App\Pkk;
+use App\RegionRule;
+use App\Role;
+use App\RoleUser;
+use App\Senbud;
+use App\UsahaMikro;
+use App\VillageRule;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -19,9 +37,46 @@ class DashboardController extends Controller
         $data_datenow = $datenow->format('Y-m-d');
         $data_datethirty = $datenow->addDays(30)->format('Y-m-d');
 
+        $countPerangkatDesa = PerangkatDesa::whereNull('deleted_at')->count();
+        $countPkk = Pkk::whereNull('deleted_at')->count();
+        $countLinmas = Linmas::whereNull('deleted_at')->count();
+        $countKt = KarangTaruna::whereNull('deleted_at')->count();
+        $countUsers = RoleUser::where('is_active', true)->count();
+        $countRoles = Role::all()->count();
+        $countUsahaMikro = UsahaMikro::whereNull('deleted_at')->count();
+        $countKerajinan = Kerajinan::whereNull('deleted_at')->count();
+        $countPerikanan = Perikanan::whereNull('deleted_at')->count();
+        $countPerkebunan = Perkebunan::whereNull('deleted_at')->count();
+        $countKuliner = Kuliner::whereNull('deleted_at')->count();
+        $countSenbud = Senbud::whereNull('deleted_at')->count();
+        $countAlam = Alam::whereNull('deleted_at')->count();
+        $countPariwisata = Pariwisata::whereNull('deleted_at')->count();
+        $countPeraturanKabupaten = RegionRule::whereNull('deleted_at')->count();
+        $countPeraturanDesa = VillageRule::whereNull('deleted_at')->count();
+        $countBerita = News::whereNull('deleted_at')->count();
+        $countPengumuman = Announcement::whereNull('deleted_at')->count();
+
         return view('admin.dashboard.index', [
             'datenow' => $data_datenow,
-            'datethirty' => $data_datethirty
+            'datethirty' => $data_datethirty,
+            'countPerangkatDesa' => $countPerangkatDesa,
+            'countPkk' => $countPkk,
+            'countLinmas' => $countLinmas,
+            'countKt' => $countKt,
+            'countPengumuman' => $countPengumuman,
+            'countBerita' => $countBerita,
+            'countPeraturanDesa' => $countPeraturanDesa,
+            'countPeraturanKabupaten' => $countPeraturanKabupaten,
+            'countPariwisata' => $countPariwisata,
+            'countAlam' => $countAlam,
+            'countSenbud' => $countSenbud,
+            'countKuliner' => $countKuliner,
+            'countPerkebunan' => $countPerkebunan,
+            'countPerikanan' => $countPerikanan,
+            'countKerajinan' => $countKerajinan,
+            'countUsahaMikro' => $countUsahaMikro,
+            'countRoles' => $countRoles,
+            'countUsers' => $countUsers,
         ]);
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -30,18 +30,27 @@
                         <div class="card-body">
                             <div class="d-flex">
                                 <div class="flex-1 overflow-hidden">
-                                    <p class="text-truncate font-size-14 mb-2">#</p>
-                                    <h4 class="mb-0"></h4>
+                                    <p class="text-truncate font-size-14 mb-2">Perangkat Desa</p>
+                                    <h4 class="mb-0">{{ $countPerangkatDesa }}</h4>
                                 </div>
                                 <div class="text-primary ms-auto">
                                     <i class="ri-team-fill font-size-24"></i>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="card-body border-top py-3">
-                            <div class="text-truncate">
-                                <a href=""><span class="text-muted ms-2">Selengkapnya <i class="mdi mdi-arrow-right"></i></span></a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">PKK</p>
+                                    <h4 class="mb-0">{{ $countPkk }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -51,17 +60,12 @@
                         <div class="card-body">
                             <div class="d-flex">
                                 <div class="flex-1 overflow-hidden">
-                                    <p class="text-truncate font-size-14 mb-2">#</p>
-                                    <h4 class="mb-0"></h4>
+                                    <p class="text-truncate font-size-14 mb-2">Linmas</p>
+                                    <h4 class="mb-0">{{ $countLinmas }}</h4>
                                 </div>
                                 <div class="text-primary ms-auto">
-                                    <i class="mdi mdi-bell-alert font-size-24"></i>
+                                    <i class="ri-team-fill font-size-24"></i>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="card-body border-top py-3">
-                            <div class="text-truncate">
-                                <a href=""><span class="text-muted ms-2">Selengkapnya <i class="mdi mdi-arrow-right"></i></span></a>
                             </div>
                         </div>
                     </div>
@@ -71,17 +75,12 @@
                         <div class="card-body">
                             <div class="d-flex">
                                 <div class="flex-1 overflow-hidden">
-                                    <p class="text-truncate font-size-14 mb-2">#</p>
-                                    <h4 class="mb-0"></h4>
+                                    <p class="text-truncate font-size-14 mb-2">Karang Taruna</p>
+                                    <h4 class="mb-0">{{ $countKt }}</h4>
                                 </div>
                                 <div class="text-primary ms-auto">
-                                    <i class="mdi mdi-chat-processing font-size-24"></i>
+                                    <i class="ri-team-fill font-size-24"></i>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="card-body border-top py-3">
-                            <div class="text-truncate">
-                                <a href=""><span class="text-muted ms-2">Selengkapnya <i class="mdi mdi-arrow-right"></i></span></a>
                             </div>
                         </div>
                     </div>
@@ -91,8 +90,259 @@
             <!-- end row -->
         </div>
     </div>
+    <hr>
+    {{-- artikel --}}
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="row">
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Pengumuman</p>
+                                    <h4 class="mb-0">{{ $countPengumuman }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Berita</p>
+                                    <h4 class="mb-0">{{ $countBerita }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <!-- end row -->
+        </div>
+    </div>
+    <hr>
 
-    {{ auth()->user()->role_id }}
+    {{-- Peraturan --}}
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="row">
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Peraturan Desa</p>
+                                    <h4 class="mb-0">{{ $countPeraturanDesa }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Peraturan Kabupaten</p>
+                                    <h4 class="mb-0">{{ $countPeraturanKabupaten }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <!-- end row -->
+        </div>
+    </div>
+    <hr>
+
+    {{-- Potensi Desa --}}
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="row">
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Pariwisata</p>
+                                    <h4 class="mb-0">{{ $countPariwisata }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Alam</p>
+                                    <h4 class="mb-0">{{ $countAlam }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Seni dan Budaya</p>
+                                    <h4 class="mb-0">{{ $countSenbud }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Kuliner</p>
+                                    <h4 class="mb-0">{{ $countKuliner }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Perkebunan</p>
+                                    <h4 class="mb-0">{{ $countPerkebunan }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Perikanan</p>
+                                    <h4 class="mb-0">{{ $countPerikanan }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Kerajinan</p>
+                                    <h4 class="mb-0">{{ $countKerajinan }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Usaha Mikro</p>
+                                    <h4 class="mb-0">{{ $countUsahaMikro }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <!-- end row -->
+        </div>
+    </div>
+    <hr>
+
+    {{-- Master Data --}}
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="row">
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Roles</p>
+                                    <h4 class="mb-0">{{ $countRoles }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-1 overflow-hidden">
+                                    <p class="text-truncate font-size-14 mb-2">Users</p>
+                                    <h4 class="mb-0">{{ $countUsers }}</h4>
+                                </div>
+                                <div class="text-primary ms-auto">
+                                    <i class="ri-team-fill font-size-24"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <!-- end row -->
+        </div>
+    </div>
     <!-- end row -->
 </div>
 @endsection
